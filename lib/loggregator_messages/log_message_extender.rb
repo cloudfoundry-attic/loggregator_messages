27e6ff8e7793454c90c7e5807c930bc92ed16da7
Glenn Oppegard <oppegard@gmail.com>
2013-09-13 11:45:58 -0600

    set timestamps as nanoseconds since the epoch; return Time object in helper
    [#56734594]

--- lib/loggregator_messages/log_message_extender.rb
+++ lib/loggregator_messages/log_message_extender.rb
@@ -10,4 +10,16 @@ class LogMessage
      SourceType::DEA => 'CF[DEA]',
      SourceType::WARDEN_CONTAINER => 'App',}[source_type]
   end
+
+  def timestamp=(timestamp)
+    timestamp ||= Time.now
+    t = timestamp.is_a?(Time) ? timestamp : Time.at(timestamp.to_f)
+    @timestamp = (t.tv_sec * 1000000000) + t.tv_nsec
+  end
+
+  def time
+    num_secs = @timestamp / 1000000000
+    fractional_usecs = (@timestamp % 1000000000).to_f / 1000
+    Time.at(num_secs, fractional_usecs)
+  end
 end
